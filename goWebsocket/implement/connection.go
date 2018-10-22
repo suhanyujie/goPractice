0d71715a6d4971ea51cdb13ce8f018568969360e
suhanyujie <suhanyujie@qq.com>
2018-10-22 23:36:25 +0800

    读取 写入消息方法

--- goWebsocket/implement/connection.go
+++ goWebsocket/implement/connection.go
@@ -3,16 +3,34 @@ package implement
 import "github.com/gorilla/websocket"
 
 type Connection struct {
-	wsConn *websocket.Conn
-	inChan chan []byte
+	wsConn  *websocket.Conn
+	inChan  chan []byte
 	outChan chan []byte
 }
 
-func InitConnection(wsConn *websocket.Conn)(*Connection, error){
-	var conn  = &Connection{
+func InitConnection(wsConn *websocket.Conn) (*Connection, error) {
+	var conn = &Connection{
 		wsConn,
-		make(chan []byte,1000),
+		make(chan []byte, 1000),
 		make(chan []byte, 1000),
 	}
 	return
+}
+
+// todo 读取消息
+func (_this *Connection) ReadMessage() (data []byte, err error) {
+	data = <-_this.inChan
+	return data, nil
+}
+
+// todo 写消息
+func (_this *Connection) WriteMessage(data []byte) (error) {
+	_this.outChan <- data
+	return nil
+}
+
+// todo
+func (_this Connection) Close()  {
+	//Close是线程安全的
+	_this.wsConn.Close()
 }
