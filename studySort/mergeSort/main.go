63a004f5976774ea53e559308fa6f498b4d8a021
suhanyujie <suhanyujie@qq.com>
2018-10-29 08:50:14 +0800

    开始堆归并排序的实现

--- studySort/mergeSort/main.go
+++ studySort/mergeSort/main.go
@@ -7,7 +7,8 @@ import "fmt"
 * 归并排序（MERGE-SORT）是利用归并的思想实现的排序方法，该算法采用经典的分治（divide-and-conquer）策略（分治法将问题分(divide)成一些小的问题然后递归求解，而治(conquer)的阶段则将分的阶段得到的各答案"修补"在一起，即分而治之)。
 
 
-
+## 参考
+* https://www.cnblogs.com/chengxiao/p/6194356.html
  */
 // 入口函数
 func main() {
