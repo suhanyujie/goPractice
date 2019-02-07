9bc3b94dd067d04d394847237cd09e61c77e7cb3
suhanyujie <suhanyujie@qq.com>
2019-02-07 15:49:28 +0800

    不定参数

---
+++ goLanguageCode/chapter2/paramNotLimit/paramNotLimit.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	func1(45,2,3);
+
+}
+
+func func1(param ...int) {
+	for _,arg := range param {
+		fmt.Println(arg)
+	}
+}
+
+/**
+## 不定参数
+* 形如`...type`格式的类型只能作为函数的参数类型存在
+
+
+
+ */
