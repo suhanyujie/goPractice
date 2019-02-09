0faf5ea6c7d75cfd7f451e7fe109afcf61203f1a
suhanyujie <suhanyujie@qq.com>
2019-02-09 14:09:20 +0800

    flag.Parse

--- goLanguageCode/chapter2/sampleSort/sorter.go
+++ goLanguageCode/chapter2/sampleSort/sorter.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var infile *string = flag.String("i","infile","File contains value for sorting")
+var outfile *string = flag.String("o","outfile","File to receive sorted values")
+var algorithm *string = flag.String("a","qsort","Sort algorithm")
+
+
 func main() {
+	flag.Parse()
+
+	if infile != nil {
+		 fmt.Println("infile = ", *infile,"outfile = ",*outfile, "algorithm = ", *algorithm)
+	}
 
 
 }
