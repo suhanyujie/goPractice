e44935ab960500e1155a057f1094e6be6ab9bbd0
suhanyujie <suhanyujie@qq.com>
2019-02-10 22:18:15 +0800

    sorter

--- goLanguageCode/chapter2/sampleSort/sorter.go
+++ goLanguageCode/chapter2/sampleSort/sorter.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"io"
+	"strconv"
 )
 
 var infile *string = flag.String("i","infile","File contains value for sorting")
@@ -16,9 +21,45 @@ func main() {
 	if infile != nil {
 		 fmt.Println("infile = ", *infile,"outfile = ",*outfile, "algorithm = ", *algorithm)
 	}
-
-
+	values,err := readValue(*infile)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(values)
 }
+
+func readValue(infile string) (values []int,err error) {
+	file,err := os.Open(infile)
+	if err!=nil {
+		log.Println("Failed to open the input file",err)
+		return
+	}
+	defer file.Close()
+	br := bufio.NewReader(file)
+	values = make([]int, 0)
+	for {
+		line,isPrefix,err1 := br.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				err = err1
+			}
+			break
+		}
+		if isPrefix {
+			fmt.Println("a too long line,seems unexpected.")
+			return
+		}
+		str := string(line)
+		value ,err1 := strconv.Atoi(str)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		values = append(values, value)
+	}
+	return
+}
+
 
 /**
 ## 排序算法
