f3ea2b6332cbdf137b0029e4c4e79253929f56dd
suhanyujie <suhanyujie@qq.com>
2019-02-11 09:38:21 +0800

    bubble sort

--- goLanguageCode/chapter2/sampleSort/algorithms/bubble/bubblesort.go
+++ goLanguageCode/chapter2/sampleSort/algorithms/bubble/bubblesort.go
@@ -1,3 +1,16 @@
 package bubble
 
-
+/**
+冒泡排序算法
+ */
+func BubbleSort(values []int) error {
+	length := len(values)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-i-1; j++ {
+			if values[j] < values[j+1] {
+				values[j], values[j+1] = values[j+1], values[j]
+			}
+		}
+	}
+	return nil
+}
--- goLanguageCode/chapter2/sampleSort/sorter.go
+++ goLanguageCode/chapter2/sampleSort/sorter.go
@@ -8,37 +8,43 @@ import (
 	"bufio"
 	"io"
 	"strconv"
+	"practice/goLanguageCode/chapter2/sampleSort/algorithms/bubble"
 )
 
-var infile *string = flag.String("i","infile","File contains value for sorting")
-var outfile *string = flag.String("o","outfile","File to receive sorted values")
-var algorithm *string = flag.String("a","qsort","Sort algorithm")
-
+var infile *string = flag.String("i", "infile", "File contains value for sorting")
+var outfile *string = flag.String("o", "outfile", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
 
 func main() {
 	flag.Parse()
 
 	if infile != nil {
-		 fmt.Println("infile = ", *infile,"outfile = ",*outfile, "algorithm = ", *algorithm)
+		fmt.Println("infile = ", *infile, "outfile = ", *outfile, "algorithm = ", *algorithm)
 	}
-	values,err := readValue(*infile)
+	values, err := readValue(*infile)
 	if err != nil {
 		log.Println(err)
 	}
+	err = bubble.BubbleSort(values)
+	if err !=nil {
+		log.Println(err)
+	}
+
 	fmt.Println(values)
 }
 
-func readValue(infile string) (values []int,err error) {
-	file,err := os.Open(infile)
-	if err!=nil {
-		log.Println("Failed to open the input file",err)
+//从文件中读取数据
+func readValue(infile string) (values []int, err error) {
+	file, err := os.Open(infile)
+	if err != nil {
+		log.Println("Failed to open the input file", err)
 		return
 	}
 	defer file.Close()
 	br := bufio.NewReader(file)
 	values = make([]int, 0)
 	for {
-		line,isPrefix,err1 := br.ReadLine()
+		line, isPrefix, err1 := br.ReadLine()
 		if err1 != nil {
 			if err1 != io.EOF {
 				err = err1
@@ -50,7 +56,7 @@ func readValue(infile string) (values []int,err error) {
 			return
 		}
 		str := string(line)
-		value ,err1 := strconv.Atoi(str)
+		value, err1 := strconv.Atoi(str)
 		if err1 != nil {
 			err = err1
 			return
@@ -60,6 +66,19 @@ func readValue(infile string) (values []int,err error) {
 	return
 }
 
+//将处理后的数据写入到文件中
+func writeValues(values []int, outfile string) (err error) {
+	file, err := os.Open(outfile)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	for _, value := range values {
+		str := strconv.Itoa(value)
+		file.WriteString(str)
+	}
+	return nil
+}
 
 /**
 ## 排序算法
@@ -69,8 +88,9 @@ func readValue(infile string) (values []int,err error) {
 USAGE: sorter –i <in> –o <out> –a <qsort|bubblesort>
 ```
 
+## 注意
+* main函数中`err = bubble.BubbleSort(values)`的调用排序函数后，无需返回数值，因为切片是引用类型，在函数内部排序后就直接影响了原切片数组
 
 
 
  */
-
