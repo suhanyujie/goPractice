a72d4b3b2d371673bdad85c523f1df471270f035
suhanyujie <suhanyujie@qq.com>
2019-02-24 16:08:43 +0800

    spider for list

--- simpleSpider/spiderServer/server.go
+++ simpleSpider/spiderServer/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"strings"
+	"fmt"
 )
 
 type InputData struct {
@@ -12,15 +13,21 @@ type InputData struct {
 	Rule string
 }
 
+type LinkData struct {
+	Text,Link string
+}
+
 type SpiderServer interface {
 	HandleReceive(text, rule string) (result string, err error)
 }
 
 type NovelServer struct {
-	DataChan *chan InputData
+	DataChan chan LinkData
 }
 
 func (server *NovelServer) HandleReceive(text, rule string, textRule []string) {
+	text = strings.TrimSpace(text)
+	fmt.Println(text)
 	textReader := strings.NewReader(text)
 	doc, err := goquery.NewDocumentFromReader(textReader)
 	if err != nil {
@@ -28,16 +35,36 @@ func (server *NovelServer) HandleReceive(text, rule string, textRule []string) {
 	}
 	ruleByte, err := base64.StdEncoding.DecodeString(rule)
 	if err != nil {
-		log.Println("spider rule decode 异常！")
+		log.Println("spider rule decode 异常:", err)
 		return
 	}
 	rule = string(ruleByte)
 	doc.Find(rule).Each(func(i int, selection *goquery.Selection) {
-
+		for _, oneRule := range textRule {
+			itemRule, err := base64.StdEncoding.DecodeString(oneRule)
+			if err != nil {
+				log.Println("spider itemRule decode 异常:", err)
+				return
+			}
+			itemRuleStr := string(itemRule)
+			aSelection := selection.Find(itemRuleStr)
+			//判断是否a链接
+			needLink := strings.HasSuffix(itemRuleStr,"a")
+			var (
+				linkData = &LinkData{"",""}
+			)
+			aSelection.Each(func(i int, s2 *goquery.Selection) {
+				linkData.Text = s2.Text()
+				if needLink {
+					linkData.Link,_ = s2.Attr("href")
+				}
+				server.DataChan <- *linkData
+			})
+		}
 	})
 }
 
 func NewNovelServer() *NovelServer {
-	ch1 := make(chan InputData, 200)
-	return &NovelServer{&ch1}
+	ch1 := make(chan LinkData, 200)
+	return &NovelServer{ch1}
 }
--- simpleSpider/spiderServer/server_test.go
+++ simpleSpider/spiderServer/server_test.go
@@ -5,25 +5,53 @@ import (
 	"log"
 	"os"
 	"testing"
+	"encoding/base64"
+	"sync"
 )
 
 func TestNovelServer(t *testing.T) {
+	var w sync.WaitGroup
 	html, err := getHtmlContent()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(html)
-	//server := NewNovelServer()
-	//server.HandleReceive("", "", []string{})
+	server := NewNovelServer()
+	defer close(server.DataChan)
+	var ruleArr = []string{
+		"dd a",
+	}
+	for index, con := range ruleArr {
+		newRule := base64.StdEncoding.EncodeToString([]byte(con))
+		ruleArr[index] = string(newRule)
+	}
+	areaRule := "#list"
+	areaRule = base64.StdEncoding.EncodeToString([]byte(areaRule))
+	go func(DataChan <-chan LinkData) {
+		w.Add(1)
+		var (
+			dataOne = &LinkData{}
+		)
+		for {
+			select {
+			case *dataOne = <-DataChan:
+				fmt.Println(dataOne.Text)
+			}
+		}
+		w.Done()
+	}(server.DataChan)
+	server.HandleReceive(string(html), areaRule, ruleArr)
+	//等待协程执行任务完成
+	w.Wait()
 }
 
 func getHtmlContent() ([]byte, error) {
-	path := "D:\\vmShare\\go\\src\\practice\\simpleSpider\\spiderServer\\test.txt"
+	path := "/www/2017/go/src/practice/simpleSpider/spiderServer/test.txt"
 	file, err := os.Open(path)
+	defer file.Close()
 	if err != nil {
 		return nil, err
 	}
-	var html []byte
+	var html = make([]byte, 1024000)
 	n, err := file.Read(html)
 	fmt.Println("the content length is:", n)
 	if err != nil {
