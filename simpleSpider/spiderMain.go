bd26e6b780e166008eeff773ac10c21b669ac80d
suhanyujie <suhanyujie@qq.com>
2019-02-25 18:42:03 +0800

    spider parseServer commit

--- simpleSpider/spiderMain.go
+++ simpleSpider/spiderMain.go
@@ -1,33 +1,82 @@
 package main
 
 import (
-	"practice/simpleSpider/spiderServer"
-	"net"
-	"log"
-	"encoding/base64"
 	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"practice/simpleSpider/spiderServer"
+	"time"
 )
 
+var server = spiderServer.NewNovelServer()
+
 func main() {
-	//启动一个tcp服务器，接收请求的数据
+	//启动一个http服务器，接收请求的数据
 	//将数据传递给server
-	server := spiderServer.NewNovelServer()
-	ln,err := net.Listen("tcp","0.0.0.0:8001")
+	http.HandleFunc("/", safeHandler(ParseHandler))
+	err := http.ListenAndServe(":8001", nil)
 	if err != nil {
-		log.Fatal("tcp server Listen error:", err)
-	}
-	defer ln.Close()
-	for {
-		conn,err := ln.Accept()
-		if err != nil {
-			log.Fatal("tcp server Accept error:", err)
-		}
-		go Handle(conn, *server)
+		log.Println("http server listen error:", err)
 	}
 }
 
-func Handle(conn net.Conn,server spiderServer.NovelServer) {
+func ParseHandler(w http.ResponseWriter, r *http.Request) {
+	//设定等待解析结果的超时时间
+	var timeout int64 = 1
+	afterTimer := make(chan int)
+	go func(ch chan int, t int64) {
+		time.Sleep(time.Duration(t) * time.Second)
+		ch <- 1
+	}(afterTimer, timeout)
+	defer r.Body.Close()
+	if r.Method != "POST" {
+		http.Error(w, "http method not allow.", http.StatusMethodNotAllowed)
+		return
+	}
+	areaRule := r.FormValue("areaRule")
+	if len(areaRule) < 1 {
+		areaRule = getAreaRule()
+	}
+	itemRuleStr := r.FormValue("itemRule")
+	fmt.Println(itemRuleStr)
+	itemRule := []string{}
+	if len(itemRuleStr) <= 0 {
+		itemRule = getItemRule()
+	} else {
+		err := json.Unmarshal([]byte(itemRuleStr), itemRule)
+		checkError(err)
+	}
+	dataStr := r.FormValue("dataStr")
+	//解析数据，放入spider server处理
+	server.HandleReceive(dataStr, areaRule, itemRule)
+
+	select {
+	case resData := <-server.DataChan:
+		_, err := w.Write([]byte(resData.Text))
+		checkError(err)
+	case <-afterTimer:
+		panic(errors.New("wait parsed result had timeout."))
+	}
+}
+
+func safeHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err, ok := recover().(error); ok {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				log.Printf("WARN:panic in %v-%v", fn, err)
+			}
+		}()
+		fn(w, r)
+	}
+}
+
+func Handle(conn net.Conn, server spiderServer.NovelServer) {
 	var (
 		//bNum int
 		dataStr string
@@ -36,9 +85,9 @@ func Handle(conn net.Conn,server spiderServer.NovelServer) {
 	//var data = make([]byte, 1024)
 	var bufReader = bufio.NewReader(conn)
 	for {
-		tmpStr,err := bufReader.ReadString('\n')
+		tmpStr, err := bufReader.ReadString('\n')
 		rAddr := conn.RemoteAddr()
-		fmt.Printf("Receive from client %s : %s \n", rAddr,dataStr)
+		fmt.Printf("Receive from client %s : %s \n", rAddr, dataStr)
 		//bNum,err = (conn).Read(data)
 		if err != nil {
 			log.Println("tcp server Accept error:", err)
@@ -59,7 +108,7 @@ func Handle(conn net.Conn,server spiderServer.NovelServer) {
 	server.HandleReceive(dataStr, getAreaRule(), getItemRule())
 }
 
-func getAreaRule() string  {
+func getAreaRule() string {
 	areaRule := "#list"
 	areaRule = base64.StdEncoding.EncodeToString([]byte(areaRule))
 	return areaRule
@@ -74,6 +123,13 @@ func getItemRule() []string {
 		ruleArr[index] = string(newRule)
 	}
 	return ruleArr
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+		return
+	}
 }
 
 //var chTask = make(chan beegoOrm.FictionOneOfList)
