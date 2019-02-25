734052d591e5d4c7ff003a133c09a53c99a0fe6e
suhanyujie <suhanyujie@qq.com>
2019-02-25 17:22:25 +0800

    go sample:add a static file server

--- goLanguageCode/chapter5/examplePhoto/photoweb.go
+++ goLanguageCode/chapter5/examplePhoto/photoweb.go
@@ -11,7 +11,64 @@ import (
 	"strconv"
 )
 
-const UPLOAD_DIR = "goLanguageCode/chapter5/examplePhoto/uploads"
+const (
+	UPLOAD_DIR = "goLanguageCode/chapter5/examplePhoto/uploads"
+	ListDir    = 0x0001
+)
+
+func main() {
+	const PORT = 8001
+	http.HandleFunc("/", safeHandler(listHandler))
+	http.HandleFunc("/upload", safeHandler(uploadHandler))
+	http.HandleFunc("/view", safeHandler(viewHandler))
+	fmt.Println("http server in " + strconv.Itoa(PORT) + ".")
+	err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+//静态资源服务器
+func staticDirHandler(mux *http.ServeMux, prefix, staticDir string, flags int) {
+	mux.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+		file := staticDir + r.URL.Path[len(prefix)-1:]
+		if (flags & ListDir) == 0 {
+			http.NotFound(w, r)
+		}
+		http.ServeFile(w, r, file)
+	})
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		renderHtml(w, "goLanguageCode/chapter5/examplePhoto/views/upload.html", nil)
+		return
+	} else if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		checkError(err)
+		filename := h.Filename
+		defer f.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		defer t.Close()
+		_, err = io.Copy(t, f)
+		checkError(err)
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
+	checkError(err)
+	var locals = make(map[string]interface{})
+	images := []string{}
+	for _, fileInfo := range fileInfoArr {
+		imageId := fileInfo.Name()
+		images = append(images, imageId)
+	}
+	locals["images"] = images
+	err = renderHtml(w, "goLanguageCode/chapter5/examplePhoto/views/list.html", locals)
+	checkError(err)
+}
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	imageId := r.FormValue("id")
@@ -21,60 +78,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-type", "image")
 	http.ServeFile(w, r, imagePath)
-}
-
-func main() {
-	const PORT = 8001
-	http.HandleFunc("/upload", safeHandler(uploadHandler))
-	http.HandleFunc("/view", safeHandler(viewHandler))
-	http.HandleFunc("/list", safeHandler(listHandler))
-	fmt.Println("http server in " + strconv.Itoa(PORT) + ".")
-	err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		renderHtml(w, "goLanguageCode/chapter5/examplePhoto/views/upload.html", nil)
-		return
-	} else if r.Method == "POST" {
-		f, h, err := r.FormFile("image")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		filename := h.Filename
-		defer f.Close()
-		t, err := os.Create(UPLOAD_DIR + "/" + filename)
-		defer t.Close()
-		if _, err := io.Copy(t, f); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
-	}
-}
-
-func listHandler(w http.ResponseWriter, r *http.Request) {
-	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	var locals = make(map[string]interface{})
-	images := []string{}
-	for _, fileInfo := range fileInfoArr {
-		imageId := fileInfo.Name()
-		images = append(images, imageId)
-	}
-	locals["images"] = images
-	err = renderHtml(w, "goLanguageCode/chapter5/examplePhoto/views/list.html", locals)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 }
 
 func renderHtml(w http.ResponseWriter, path string, locals map[string]interface{}) error {
