259d1ab43468cbb025b88ca00a4ca3b7956afcab
suhanyujie <suhanyujie@qq.com>
2019-02-27 18:48:22 +0800

    add a option for gitignore

--- goLanguageCode/other/json/json.go
+++ goLanguageCode/other/json/json.go
@@ -1,1 +1,15 @@
-package json
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	var str1 = "{\"0\":\"who are u\"}"
+	data := []string{}
+	err := json.Unmarshal([]byte(str1), &data)
+	fmt.Println(err)
+	fmt.Println(data)
+
+}
