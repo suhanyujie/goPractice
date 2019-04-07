39cd18d41748be7331b816a7d10358a19ea77aec
suhanyujie <suhanyujie@qq.com>
2019-04-07 21:05:07 +0800

    开始最长回文子串 longestPalindrome

---
+++ leetcode/middle/longestPalindrome.go
@@ -0,0 +1,18 @@
+package main
+
+import "log"
+
+func main() {
+	TestLongestPalindrome()
+}
+
+func longestPalindrome(s string) string {
+
+	return ""
+}
+
+func TestLongestPalindrome() {
+	var exmStr = ""
+	resStr := longestPalindrome(exmStr)
+	log.Println(resStr)
+}
