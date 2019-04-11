ada4b3e573d62f485c5d37bfb9cd1d8fb564a0ba
suhanyujie <suhanyujie@qq.com>
2019-04-11 17:07:34 +0800

    leetcode-最长回文子串

--- htmlParserServer/README.md
+++ htmlParserServer/README.md
@@ -5,7 +5,7 @@
 * 解析 `dom` 使用 `goquery`
 
 ## build
-* 在仓库根目录下，`go build -o a.exe simpleSpider/spiderMain.go`
+* 在仓库根目录下，`go build -o htmlParseServer main.go`
 * 执行`./a.exe`即可启动服务
 
 ## benchamark
--- leetcode/middle/longestPalindrome.go
+++ leetcode/middle/longestPalindrome.go
@@ -1,14 +1,57 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
+/**
+最长回文子串 https://leetcode-cn.com/problems/longest-palindromic-substring/solution/
+*/
 func main() {
-	TestLongestPalindrome()
+	s1 := "aabcba"
+	lStr := longestPalindrome(s1)
+	log.Println(lStr)
+	//TestLongestPalindrome()
 }
 
 func longestPalindrome(s string) string {
+	if len(s) < 1 {
+		return ""
+	}
+	l1 := len(s)
+	start := 0
+	end := 0
+	compareLen := 0
+	maxLen := 0
+	len1 := 0
+	len2 := 0
+	for i := 0; i < l1; i++ {
+		len1 = expandAroundCenter(s, i, i)
+		len2 = expandAroundCenter(s, i, i+1)
+		if len1 > len2 {
+			compareLen = len1
+		} else {
+			compareLen = len2
+		}
+		if compareLen >= maxLen {
+			start = i - (compareLen-1)/2
+			end = i + compareLen/2
+			maxLen = compareLen
+		}
+	}
 
-	return ""
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, l, r int) int {
+	L := l
+	R := r
+	for L >= 0 && R < len(s) && s[L] == s[R] {
+		L--
+		R++
+	}
+
+	return R - L - 1
 }
 
 func TestLongestPalindrome() {
